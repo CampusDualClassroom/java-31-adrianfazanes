22b03aa60e35863ad823b414bb21fd203c838999
Adrián Fazanes Porto <adrifazanes38@gmail.com>
2024-09-27 10:32:24 +0200

    Ejercicio 31 Resuelto

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,19 @@
 package com.campusdual.classroom;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Exercise31 {
 
     public static void main(String[] args) {
-
+        try (FileReader fr = new FileReader("src/main/resources/lorem.txt")) {
+            BufferedReader br = new BufferedReader(fr);
+            String linea;
+            while((linea=br.readLine())!=null)
+                System.out.println(linea);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
